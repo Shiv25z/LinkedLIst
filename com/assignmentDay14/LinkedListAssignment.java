fe6cf3de3b299db4bead177f1198571af9f343da
Shiv25z <shivamzagade25899@gmail.com>
2022-10-08 20:16:26 +0530

    Welcome to Linked List Assignment!

---
+++ com/assignmentDay14/LinkedListAssignment.java
@@ -0,0 +1,9 @@
+package com.assignmentDay14;
+
+import java.util.LinkedList;
+
+public class LinkedListAssignment {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Assignment");
+    }
+}
