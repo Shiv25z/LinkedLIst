696f233637bcd55f4363a08cf1f1b30a578afc75
Shiv25z <shivamzagade25899@gmail.com>
2022-10-12 18:15:47 +0530

    Added: Insert method to append new Nodes

--- com/assignmentDay14/LinkedListAssignment.java
+++ com/assignmentDay14/LinkedListAssignment.java
@@ -2,60 +2,74 @@ package com.assignmentDay14;
 
 
 
-public class LinkedListAssignment {
-   static Node head;
-    static Node tail;
-    static class Node{
-        int data;
+public class LinkedListAssignment <T>{
+    static Node head;
+    Node tail;
+    static class Node<T>{
+        T data;
         Node next;
 
-        public Node(int data) {
+        public Node(T data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    public static LinkedListAssignment append(LinkedListAssignment list, int data){
-
+    public static void append(int data){
         Node new_node = new Node(data);
-
-
-        if (list.head == null) {
-            list.head = new_node;
+        if (head == null) {
+            head = new_node;
         }
         else {
 
-            new_node.next = head;
-            head = new_node;
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+
+            last.next = new_node;
         }
 
-        return list;
     }
 
 
     public static void printList(LinkedListAssignment list) {
+        if(list.head == null){
+            System.out.println("LinkedList is empty");
+        }
         Node currNode = list.head;
 
         System.out.print("LinkedList: ");
 
         while (currNode != null) {
-
             System.out.print(currNode.data + " -> ");
-
             currNode = currNode.next;
         }
         System.out.println("NULL");
     }
-
+    public void insert(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node<T> temp, current;
+            current = head;
+            temp = head.next;
+            current.next = newNode;
+            newNode.next = temp;
+        }
+    }
 
     public static void main(String[] args){
         System.out.println("Welcome to Linked List Assignment");
         LinkedListAssignment list = new LinkedListAssignment();// UC2
 
 
-        list = append(list, 70);//UC3
-        list = append(list, 30);
-        list = append(list, 56);
+        list.append( 70);//UC3
+        list.append(56);
+        list.insert(30);//UC4
 
         printList(list);
     }
